777b597ae9bd9e8d111102dd9ed019494242f7e0
Buse Çoban <73944611+busecoban@users.noreply.github.com>
2024-09-16 21:57:33 +0300

    2-add two numbers

--- addTwoNumbers_2.java
+++ addTwoNumbers_2.java
@@ -1,5 +1,41 @@
 public class addTwoNumbers_2 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p=l1, q=l2, curr=dummyHead;
+        int carry=0;
+        
+        while(p!=null || q!=null){
+            int x=(p!=null)?p.val:0;
+            int y=(q!=null)?q.val:0;
+            int sum=carry+x+y;
+            carry=sum/10;
+            curr.next=new ListNode(sum%10);
+            curr=curr.next;
+            if(p!=null) p=p.next;
+            if(q!=null) q=q.next;
+        }
+        if(carry>0){
+            curr.next=new ListNode(carry);
+        }
+        return dummyHead.next;
         
     }
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+        
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+        
+        addTwoNumbers_2 obj = new addTwoNumbers_2();
+        ListNode result = obj.addTwoNumbers(l1, l2);
+        
+        while(result!=null){
+            System.out.print(result.val + " ");
+            result=result.next;
+        }
+      
+}
 }
