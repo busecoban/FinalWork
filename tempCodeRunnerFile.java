3b5040f5a4c86a9ae73188df776192e44df64293
Buse Çoban <73944611+busecoban@users.noreply.github.com>
2024-10-21 05:40:53 +0300

    .

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,24 +1,19 @@
-public class IntegerToRoman12 {
-    public String intToRoman(int num) {
-        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 
-                        10, 9, 5, 4, 1};
-        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL",
-                            "X", "IX", "V", "IV", "I"};
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < values.length; i++) {
-            while (num >= values[i]) {
-                num -= values[i];
-                sb.append(symbols[i]);
-            }
-        }
-        return sb.toString();
-    }
+import java.util.Arrays;
 
+public class FindArrGivenSubsetSums_1982 {
+    public int[] recoverArray(int n, int[] sums) {
+        int m = sums.length;
+        Arrays.sort(sums);
+        int[] res = new int[m];
+        for (int i = 0; i < m; i++) {
+            res[i] = sums[i];
+        }
+        return res;
+    }
     public static void main(String[] args) {
-        IntegerToRoman12 sol = new IntegerToRoman12();
-        int num = 3;
-        System.out.println("intToRoman: "+sol.intToRoman(num));
+        FindArrGivenSubsetSums_1982 obj = new FindArrGivenSubsetSums_1982();
+        int n = 3;
+        int[] sums = {1, 2, 3, 4, 5, 6};
+        System.out.println("Recover array: " + Arrays.toString(obj.recoverArray(n, sums)));
     }
 }
-
-
