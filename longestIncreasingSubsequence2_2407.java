841139af231d0c9cc6ce5fb56a3b15ae3f7c4b84
Buse Çoban <73944611+busecoban@users.noreply.github.com>
2024-10-21 05:48:27 +0300

    problem 2407

---
+++ longestIncreasingSubsequence2_2407.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class longestIncreasingSubsequence2_2407 {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+
+        int maxLength = 1;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        longestIncreasingSubsequence2_2407 lis = new longestIncreasingSubsequence2_2407();
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        System.out.println("Length of Longest Increasing Subsequence: " + lis.lengthOfLIS(nums));
+    }
+}
